61e389880f6c7196a21a78861fbb2a89bbfd0a81
Jaroslav Schnaubert <jardaschnaubert@seznam.cz>
2020-05-26 09:38:52 +0200

    Finished setion 7

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,19 @@ const User = require("../modules/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
 
 // @route     GET api/auth
 // @desc      Get logged in user
 // @access    Private
-router.get("/", (req, res) => {
-  res.send("Logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
 });
 
 // @route     POST api/auth
